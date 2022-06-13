6141dc82d4c1791ccd949c194d97517f7ccefa19
MhmdWishah <mhmdwishah108@gmail.com>
2022-06-13 20:20:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './pages/Home/Home';
+import { Container } from './global.style';
+function App() {
+  return (
+    <Container>
+      <Home />
+    </Container>
+  );
+}
+
+export default App;
