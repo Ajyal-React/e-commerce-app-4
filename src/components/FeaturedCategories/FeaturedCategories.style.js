fafda364d4ccc118f3d2db970a96002eb2fedea9
AmiraAliJa1282001 <aa.mm.ii.rr.aa.1282001@gmail.com>
2022-06-22 15:55:11 +0200

    first copy

--- src/components/FeaturedCategories/FeaturedCategories.style.js
+++ src/components/FeaturedCategories/FeaturedCategories.style.js
@@ -1,1 +1,105 @@
 import styled from "styled-components";
+import Image from "./../../global.style";
+
+export const FeaturedCat = styled('div')`
+overflow-x: hidden; 
+margin-top:50px;
+`
+
+export const CategoriContainer = styled('div')`
+width: 80vw;
+margin: auto;
+margin-bottom: 20px;
+  
+`
+export const CategoriNav = styled('div')`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    border-bottom: 1px solid #504f4f;
+`
+export const CategoriList = styled('li')`
+    list-style-type: none;
+    display: inline-block;
+    padding: 10px;
+    margin-top: 25px;
+`
+export const CategoriListLink = styled('a')`
+    text-decoration: none;
+    color: #504f4f;
+`
+export const CategoriListButton = styled('button')`
+    background-color: #FA7400;
+    border: none;
+    color: white;
+    padding: 10px;
+    border-radius: 10px;
+`
+export const Categorilogo = styled('div')`
+background: url("/images/Group 14850.png");
+
+`
+export const MobileSec = styled('div')`
+margin-top: 50px;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+`
+export const ItemsCat = styled('div')`
+width: 58%;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+row-gap: 15px;
+`
+export const CatagoriItem = styled('div')`
+    width: 320px;
+    border: 1px solid #808080;
+    border-radius: 10px;
+    background-color: #F9F8F8;
+`
+export const ItemImage = styled('Image')`
+width: 100%;
+`
+export const Itemtitel = styled('div')`
+display: flex;
+justify-content: space-between;
+width: 80%;
+margin: 0 auto;
+margin-bottom: 36px;
+border-bottom: 2px solid #9c9191;
+`
+export const ItemName = styled('p')`
+color: #111111;
+font-weight: 700;
+`
+export const ItemPrice = styled('span')`
+        padding: 5px;
+        background-color: white;
+        margin-bottom: 5px;
+        color: #7CD0AB;
+`
+export const SliderCatagori = styled('div')`
+width: 40%;
+`
+export const CatagoriItemSlider = styled('div')`
+    width: 100%;
+    height: 100%;
+    border: 1px solid #808080;
+    border-radius: 10px;
+    background-color: #F9F8F8;
+`
+export const Sliderimage = styled('Image')`
+width: 100%;
+
+`
+
+
+
+
+
+
+
+
+
+
