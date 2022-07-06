ea8e5233b90901e1cf816cf3f1114b9d6c0fbf75
Bilal Alnajjar <75515936+BilalAlnajjar@users.noreply.github.com>
2022-07-06 14:07:05 +0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,21 +5,30 @@ import Hero from '../../components/Hero/Hero';
 import Nav from '../../components/Nav/Nav';
 import { Container, MainButton } from '../../global.style';
 import TrendingProducts from "../../components/TrendingProducts/TrendingProducts";
-import FooterComponent from '../../components/FooterComponent/FooterComponent'
-
+import {
+  useNavigate
+} from "react-router-dom";
+import { UserStateSelector } from '../../redux/user/user.selectors';
 
 function Home() {
+  const navigate = useNavigate();
+  const userData = UserStateSelector();
+
+  React.useEffect(() => {
+    if(!userData){
+        navigate('/Auth');
+    }
+  }, [userData]);
+  
   return (
     // <div>Home.component</div>
     <Container>
       <Nav/>
       <Hero/>
-       <FeaturedCategories/>
-      {/* <FeaturedProducts/> */}
-        <TrendingProducts />
-        <FooterComponent/>
+      <FeaturedCategories/>
+      <FeaturedProducts/>
+      <TrendingProducts />
     </Container>
-    
   )
 }
 
